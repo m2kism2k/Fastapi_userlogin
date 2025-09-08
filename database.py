b4b233557406b391122441eb12394982c38d5b4a
Min_Khant_Kyaw <125483583+m2kism2k@users.noreply.github.com>
2025-09-08 20:30:47 +0630

    Update database.py
    added foerign keys

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
+from sqlalchemy import Column, Integer, ForeignKey, String, create_engine
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
+
 
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mkk163@localhost:5432/user_registration"
 
@@ -8,15 +10,38 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+class Department(Base):
+    __tablename__ = "departments"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+
+    users = relationship("User", back_populates="department")
+
+class Role(Base):
+    __tablename__ = "roles"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+
+    users = relationship("User", back_populates="role")
+
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    department = Column(String, nullable=True)
-    role = Column(String, nullable=True)
 
-Base.metadata.create_all(bind=engine)
+    # FKs
+    department_id = Column(Integer, ForeignKey("departments.id"))
+    role_id = Column(Integer, ForeignKey("roles.id"))
+
+    # Relationships
+    department = relationship("Department", back_populates="users")
+    role = relationship("Role", back_populates="users")
+
+
+# Create tables
+def init_db():
+    Base.metadata.create_all(bind=engine)
 
 def get_db():
     db = SessionLocal()
